0aa71a075bc324d5ae5792772dafbbe92873d2c8
ppegu <ppegu@cisco.com>
2023-10-17 10:18:22 +0530

    modifications -done

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("GAND");
+    case mBTC: return QString("mGAND");
+    case uBTC: return QString::fromUtf8("μGAND");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Gandercoins");
+    case mBTC: return QString("Milli-Gandercoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Gandercoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
--- src/validation.cpp
+++ src/validation.cpp
@@ -1170,7 +1170,12 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
+    CAmount nSubsidy;
+
+    if(nHeight == 1) nSubsidy = 100000000 * COIN; // 100m pre-mined
+    else nSubsidy = 55 * COIN;
+
+    
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
